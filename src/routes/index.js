2a2ec64a3d80787919175f02565562fbb85c82d0
rep0land <eacosta3572@gmail.com>
2024-01-30 22:45:23 -0400

    first commit

---
+++ src/routes/index.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const router = express.Router();
+const userRouter = require('./user.router')
+
+// colocar las rutas aquí
+
+router.use(userRouter);
+
+module.exports = router;
